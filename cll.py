3f9c16510f3516d66e3cd195ebb49fc3b93379a4
SarthakKamboj <sarthakkamboj22@gmail.com>
2020-06-27 23:45:12 -0700

    added cll and cll prepend function

--- cll.py
+++ cll.py
@@ -42,10 +42,30 @@ class CircularLinkedList:
         cur.next = new_node
         new_node.next = self.head
 
+    def prepend(self, data):
+        if not self.head:
+            self.head = Node(data)
+            self.head.next = self.head
+            return
+        new = Node(data)
+        # A -> B -> C -> D -> A
+        orig_head = self.head
+        new.next = orig_head
+        self.head = new
+        last = orig_head
+        while last.next != orig_head:
+            last = last.next
+        last.next = self.head
+
+    def delete(self, data):
+        pass
+
 
 cll = CircularLinkedList()
-cll.append('A')
+
 cll.append('B')
 cll.append('C')
 cll.append('D')
+cll.prepend("A")
+
 cll.print_list()
